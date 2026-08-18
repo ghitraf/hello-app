747be2dded2a8c6f7e27141aeb24033c4f10c5e5
ghitraf <pryranda0430@gmail.com>
2026-08-18 10:14:26 +0000

    feat: test poll scm otomatis

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World v3 - Poll SCM otomatis! - Deploy OTOMATIS oleh Jenkins!\n');
+    res.end('Hello World v4 - Poll SCM Otomatis!
 });
 
 server.listen(port, hostname, () => {
